d7550d616b4d68d415f91c81c01eba5a85b90043
Mehedi <mehedi.interface@gmail.com>
2026-01-27 23:24:58 +0600

    js string

---
+++ string.js
@@ -0,0 +1,20 @@
+// const country = "Bangladesh"; 
+// const division = "Chattagram"; 
+// const district = "Feni"; 
+// const thana = "Dagonbhuyian"; 
+
+// console.log(thana); 
+
+
+const numbers = [54, 98, 78, 21, 65]; 
+console.log(numbers); 
+numbers[1] = 11; 
+console.log(numbers); 
+
+const capital = "Dhaka"; 
+console.log(capital.length); 
+capital[0] = 'F'; 
+console.log(capital); 
+
+
+
