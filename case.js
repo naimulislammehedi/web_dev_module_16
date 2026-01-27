8f7660fcbbb92684b948430f4c92d1d1d0c83011
Mehedi <mehedi.interface@gmail.com>
2026-01-28 00:16:33 +0600

    16_2 String Comparison lowercase uppercase and trim

---
+++ case.js
@@ -0,0 +1,27 @@
+const school = "RAJ UK Uttara Model School"; 
+console.log(school.toLowerCase()); 
+console.log(school.toUpperCase()); 
+
+// console.log(school); 
+
+const subject = 'Chemistry'; 
+const book = 'chemistry'; 
+
+if (subject === book) {
+    console.log("I'm reading book aibar porikkhai fatai felmu"); 
+}
+else {
+    console.log("hudai hudai pristha ultai r vat khaoar jonno.")
+} 
+
+
+const drink = 'water'; 
+const liquid = '      water    '; 
+console.log(drink.length); 
+console.log(liquid.length); 
+
+if (drink.trim() === liquid.trim()) {
+    console.log('pani er opor nam life'); 
+} else {
+    console.log('somudre pani ase khaite pari na.')
+}
