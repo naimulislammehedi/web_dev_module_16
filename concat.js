7ca9be0e146d87f203648eb0b42dda87edfaf0d7
Mehedi <mehedi.interface@gmail.com>
2026-01-28 01:09:06 +0600

    16_3 String Slice, Join, Concat and includes

---
+++ concat.js
@@ -0,0 +1,8 @@
+const first = 'Abid'; 
+const last = 'Nabid'; 
+
+// const fullName = first + " " + last; 
+const fullName = first.concat(' ').concat(last); 
+console.log(fullName); 
+
+console.log(fullName.includes('d')); 
---
+++ slice.js
@@ -0,0 +1,18 @@
+const address = "andorKilla"; 
+const part = address.slice(2, 5); 
+console.log(part); 
+
+// 
+const sentence = 'I am a good and hardworking person'; 
+// console.log(sentence.split('')); 
+// console.log(sentence.split(' ')); 
+// console.log(sentence.split('a')); 
+
+// const friendStr = 'Rahim, Kahim, Dahim, Lahim, Fahim, Sahim'; 
+// const friends = friendStr.split(','); 
+// console.log(friends);
+
+const realFriend = ['rahim', 'kahim', 'dahim', 'lahim', 'fahim']; 
+console.log(realFriend.join()); 
+console.log(realFriend.join("|")); 
+console.log(realFriend.join("-")); 
