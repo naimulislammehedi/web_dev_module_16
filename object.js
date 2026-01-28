4a79fd5cdad7d5c6ee904525b61742e579c6330f
Mehedi <mehedi.interface@gmail.com>
2026-01-28 15:51:39 +0600

    16_5 Introduction to Objects properties and values

---
+++ object.js
@@ -0,0 +1,19 @@
+// primitive 
+const bottle = {
+    brand: 'apple', 
+    price: 45, 
+    color: 'white', 
+    isClean: false
+}
+
+// non-priitive 
+const subject = {
+    name: 'biology', 
+    teacher: 'rasheda mam', 
+    examDate: '30 Feb', 
+    chapters: ['first', 'second', 'third'], 
+    exams: {
+        name: 'Final Exam', 
+        marks: 100
+    }
+}
